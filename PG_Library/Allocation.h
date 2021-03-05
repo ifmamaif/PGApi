da8794176c0905c4218611b8205f68dbbb55b9ff
Marius Ifrim <ifmamaif@gmail.com>
2021-03-05 19:11:33 +0200

    Created Macro to get list from unknown parameter list

--- PG_Library/Allocation.h
+++ PG_Library/Allocation.h
@@ -1,7 +1,17 @@
 #pragma once
 
-
 //void* Alloc(size_t size, unsigned char dimensions, ...);
 //
 //#define PG_Alloc(restultType,basicType,dimensions, ...) \
 //    (restultType)Alloc(sizeof(basicType,dimessions,...))
+
+#define VA_LIST_GET(number,type,out_pointer)        \
+    {                                               \
+        va_list list;                               \
+        va_start(list, number);                     \
+        for (int i = 0; i < number; i++)            \
+        {                                           \
+            out_pointer[i] = va_arg(list, type);    \
+        }                                           \
+        va_end(list);                               \
+     }                                              \
